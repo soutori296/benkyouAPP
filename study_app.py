ec5a2e303293db719d5561d90e2d24eb935adb7e
soutori296 <soutori296@gmail.com>
2026-03-16 13:38:41 +0900

    Update study_app.py

--- study_app.py
+++ study_app.py
@@ -246,7 +246,7 @@ def get_kanji_score(canvas_result, char, correct_strokes):
         else 0
     )
 
-    if f_score > 0.50:
+    if f_score > 0.70:
         return 100
     if f_score > 0.15:
         return 34
